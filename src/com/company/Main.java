c4005f86fb4dfb70fb32e66996774ab376cf9078
Stefan <s195467@student.dtu.dk>
2019-10-02 20:55:29 +0200

    Terningkast method og oprydning

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,8 +7,15 @@ public class Main {
 
     public static void main(String[] args) {
         //Indsættelse af variabler
-        int max, min, runde, diceRollOne, diceRollTwo, totalDice, p1Point, p2Point;
+        int max, min, runde, p1Point, p2Point;
         String player1, player2;
+
+        //Variablernes værdier bestemmes
+        p1Point=0;
+        p2Point=0;
+        max=6;
+        min=1;
+        runde = 1;
 
         System.out.println("Velkommen til terning spillet, indtast hver jeres navn og tryk enter!");
 
@@ -17,13 +24,19 @@ public class Main {
         player1 = scan.nextLine();
         player2 = scan.nextLine();
 
-        //Variablernes værdier bestemmes
-        p1Point=0;
-        p2Point=0;
-        max=6;
-        min=1;
-        runde = 0;
 
-
+        }
     }
-}
+
+        //Terningkast der returnerer en array af begge kast
+        static int[] terningKast() {
+            int diceRollOne, diceRollTwo;
+            int[] diceHitArray;
+            Random rand = new Random();
+            diceRollOne = rand.nextInt(6) + 1;
+            diceRollTwo = rand.nextInt(6) + 1;
+            diceHitArray = new int[]{diceRollOne, diceRollTwo};
+            return diceHitArray;
+        }
+    }
+
