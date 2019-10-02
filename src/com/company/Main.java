31ca314090a6df85adff19d6835ec3466e4f919d
Stefan <s195467@student.dtu.dk>
2019-10-02 19:19:33 +0200

    Import af pakker og spillernavne

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,20 @@
 package com.company;
+import java.util.Scanner;
+import java.lang.Math;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
         //Indsættelse af variabler
-        int max, min, runde, diceRollOne, diceRollTwo, totalDice, point;
+        int max, min, runde, diceRollOne, diceRollTwo, totalDice, p1Point, p2Point;
+        String player1, player2;
+
+        System.out.println("Velkommen til terning spillet, indtast hver jeres navn og tryk enter!");
+
+        //Importerer en scanner og sætter spillernes navn ud fra scannerens input
+        Scanner scan = new Scanner(System.in);
+        player1 = scan.nextLine();
+        player2 = scan.nextLine();
     }
 }
